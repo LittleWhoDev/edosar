a5534110f0946a482a750657678d5cd050055b1f
JustBeYou <mihailferaru2000@gmail.com>
2020-11-01 00:48:40 +0200

    mare boss

---
+++ client/src/api/acte.js
@@ -0,0 +1,1 @@
+export const downloadPath = "http://localhost:3000/acte";
--- client/src/components/DosarPreview/DosarPreview.js
+++ client/src/components/DosarPreview/DosarPreview.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Dialog, Box } from '@material-ui/core';
 import { statusToString } from "api/dosar"
+import { getDosar } from 'api/cetatean';
+import { downloadPath } from 'api/acte';
 
 function DosarPreview({ dosar, onClose, open }) {
+  const [dosarFull, setDosarFull] = useState({ sablon: { necesare: [] } });
+
+  useEffect(() => {
+    if (!open) return;
+    getDosar(dosar._id).then((d) => { setDosarFull(d) });
+  }, [open])
+
   return (
     <Dialog open={open} onClose={onClose}>
       <Box>
@@ -12,6 +21,15 @@ function DosarPreview({ dosar, onClose, open }) {
         <Typography variant="body1">Denumire: {dosar.name} </Typography>
         <Typography variant="body1">Stadiu: {statusToString(dosar.status)} </Typography>
         <Typography variant="body1">Data: {dosar.createdAt} </Typography>
+        {
+          dosarFull.sablon.necesare.map((elem) => {
+            if (elem.type === "text") return <Typography variant="body1">{elem.name}: {dosarFull[elem.name]} </Typography>
+            if (elem.type === "file") {
+              return <Typography variant="body1">{elem.name}: <a href={`${downloadPath}/${dosarFull[elem.name]}`}> Vizualizeaza document </a> </Typography>
+            }
+            return <></>
+          })
+        }
       </Box>
     </Dialog>
   );
