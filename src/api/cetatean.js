fb5291eb1c419a8d21a2dce3bd4ea829f5f8c576
JustBeYou <mihailferaru2000@gmail.com>
2020-11-01 00:48:40 +0200

    mare boss

--- src/api/cetatean.js
+++ src/api/cetatean.js
@@ -3,11 +3,16 @@ import axios from "axios";
 const apiBase = "http://localhost:3000";
 
 export async function getDosare() {
-    const r = await axios.get(`${apiBase}/dosar`);
-    return r.data;
+  const r = await axios.get(`${apiBase}/dosar`);
+  return r.data;
 }
 
 export async function getStatistics() {
   const r = await axios.get(`${apiBase}/dosar/count`);
+  return r.data;
+}
+
+export async function getDosar(id) {
+  const r = await axios.get(`${apiBase}/dosar/${id}`);
   return r.data;
 }
