f9fbaf97ceac2a9683dd1130ffa76e2db52fa44b
Seras <50520077+Seras3@users.noreply.github.com>
2020-10-31 22:22:09 +0200

    feat: adopt initial theme (#1)
    * feat: adopt initial theme
    
    * feat: update dashboard
    
    * feat: keep side utils and tables
    
    * feat: edit general layout

---
+++ src/views/Dashboard/DashboardPrimarie.js
@@ -0,0 +1,138 @@
+import React from "react";
+// @material-ui/core
+import { makeStyles } from "@material-ui/core/styles";
+import Icon from "@material-ui/core/Icon";
+// @material-ui/icons
+import Store from "@material-ui/icons/Store";
+import Warning from "@material-ui/icons/Warning";
+import DateRange from "@material-ui/icons/DateRange";
+import LocalOffer from "@material-ui/icons/LocalOffer";
+import Update from "@material-ui/icons/Update";
+import Accessibility from "@material-ui/icons/Accessibility";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+import Danger from "components/Typography/Danger.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardIcon from "components/Card/CardIcon.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+
+import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+import { Button, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles(styles);
+
+export default function Dashboard() {
+  const classes = useStyles();
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <Icon>content_copy</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Depuse</p>
+              <h3 className={classes.cardTitle}>
+                49/50 
+              </h3>
+            </CardHeader>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Store />
+              </CardIcon>
+              <p className={classes.cardCategory}>In asteptare</p>
+              <h3 className={classes.cardTitle}>
+                49/50 
+              </h3>
+            </CardHeader>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Acceptate</p>
+              <h3 className={classes.cardTitle}>
+                49/50 
+              </h3>
+            </CardHeader>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Accessibility />
+              </CardIcon>
+              <p className={classes.cardCategory}>Respinse</p>
+              <h3 className={classes.cardTitle}>
+                49/50 
+              </h3>
+            </CardHeader>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="warning">
+              <h4 className={classes.cardTitleWhite}>Ultimele dosare</h4>
+            </CardHeader>
+            <CardBody>
+              <Table
+                tableHeaderColor="warning"
+                tableHead={["ID", "Name", "Salary", "Country"]}
+                tableData={[
+                  ["1", "Dakota Rice", "$36,738", "Niger"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
+                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
+                ]}
+              />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color="info">
+              <h4 className={classes.cardTitleWhite}>Contact primarie</h4>
+            </CardHeader>
+            <CardBody>
+              <Typography variant="body1">
+                Bucuresti,
+                <br/>
+                Sector 3,
+                <br/>
+                Strada Carol Davila Nr.55
+              </Typography>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer >
+          <GridItem xs={12} sm={12} md={12} >
+            <Card>
+              <CardBody>
+                <Button color="primary" variant="contained" style={{ width: '100%', height:'3rem'}} >
+                  Adauga document
+                </Button>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
+    </div>
+  );
+}
