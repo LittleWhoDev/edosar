67cabcab1398bca2660fb694bb175fc3f0beba9d
JustBeYou <mihailferaru2000@gmail.com>
2020-11-01 01:24:07 +0200

    florin salam solutioneaza dosare

--- src/api/acte.js
+++ src/api/acte.js
@@ -1,1 +1,15 @@
+import axios from "axios";
+
 export const downloadPath = "http://localhost:3000/acte";
+const apiBase = "http://localhost:3000";
+
+export async function uploadAct(f) {
+    const formData = new FormData();
+    formData.append('act', f)
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    }
+    return (await axios.post(`${apiBase}/act`, formData, config)).data
+}
