df1521eb5d92ede17deb9e407ec464f24e59abb1
JustBeYou <mihailferaru2000@gmail.com>
2020-11-01 11:15:57 +0200

    porturi, da nu cu nave

--- src/api/primarie.js
+++ src/api/primarie.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBase = "http://localhost:3000";
+const apiBase = "http://localhost:5005";
 
 export async function updateDosar(id, data) {
     const r = await axios.put(`${apiBase}/dosar/${id}`, data);
